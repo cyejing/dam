223a75e1a28d2a69cbf7916c726474ce53f11794
cyejing <jing23501@qq.com>
2021-10-10 12:10:01 +0800

    add rewrite it

--- dam-integration-test/src/test/java/cn/cyejing/dam/it/smoke/HealthFilterIT.java
+++ dam-integration-test/src/test/java/cn/cyejing/dam/it/smoke/HealthFilterIT.java
@@ -2,6 +2,7 @@ package cn.cyejing.dam.it.smoke;
 
 import cn.cyejing.dam.core.container.DamContainer;
 import cn.cyejing.dam.it.base.BaseIT;
+import lombok.extern.slf4j.Slf4j;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.ListenableFuture;
 import org.asynchttpclient.RequestBuilder;
@@ -12,17 +13,21 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+@Slf4j
 public class HealthFilterIT {
 
     private DamContainer damContainer;
 
     @Before
     public void setUp() {
+        log.info("setUp");
         damContainer = BaseIT.startCore();
         damContainer.start();
     }
+
     @After
-    public void tearDown() {
+    public void shutdown() {
+        log.info("shutdown");
         damContainer.shutdown();
     }
 
--- dam-integration-test/src/test/java/cn/cyejing/dam/it/smoke/RewriteFilterIT.java
+++ dam-integration-test/src/test/java/cn/cyejing/dam/it/smoke/RewriteFilterIT.java
@@ -3,6 +3,7 @@ package cn.cyejing.dam.it.smoke;
 import cn.cyejing.dam.core.container.DamContainer;
 import cn.cyejing.dam.it.base.BaseIT;
 import cn.cyejing.dam.it.server.WebServer;
+import lombok.extern.slf4j.Slf4j;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.RequestBuilder;
 import org.asynchttpclient.Response;
@@ -14,6 +15,7 @@ import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertEquals;
 
+@Slf4j
 public class RewriteFilterIT {
 
     private DamContainer damContainer;
@@ -22,6 +24,7 @@ public class RewriteFilterIT {
 
     @Before
     public void setUp() {
+        log.info("setUp");
         webServer = new WebServer();
         damContainer = BaseIT.startCore();
 
@@ -31,6 +34,7 @@ public class RewriteFilterIT {
 
     @After
     public void shutdown() {
+        log.info("shutdown");
         webServer.shutdown();
         damContainer.shutdown();
     }
